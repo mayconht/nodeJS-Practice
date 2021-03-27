c8ffd44e8e1c59ac391182ae9f47b06b1ce09da4
Maycon Douglas <mdss@br.ibm.com>
2021-03-27 16:39:21 -0300

    Estudo de listas For in, for e For of

---
+++ 3 - listas/for - forin - forof/service.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+const URL = 'https://swapi.dev/api/people'
+
+//Fara a busca no site swapi que traz dados dos personagens do star wars
+
+async function getPeople(name) {
+    const url = `${URL}/?search=${name}&format=json`
+    const response = await axios.get(url)
+    return response.data
+}
+
+module.exports = {
+    getPeople
+}
+
+
+
+// getPeople('r2')
+//     .then(function (res) {
+//         console.log('result', res)
+//     })
+//     .catch(function (error) {
+//         console.error('erro', error)
+//     })
