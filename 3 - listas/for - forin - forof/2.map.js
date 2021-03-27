36458b95d7b250e050787c6da98fdbde354196b3
Maycon Douglas <mdss@br.ibm.com>
2021-03-27 16:59:57 -0300

    Trabalhando com Map (rescrevendo a função MAP de Array Prototype)

---
+++ 3 - listas/for - forin - forof/2.map.js
@@ -0,0 +1,41 @@
+const service = require('./service')
+
+
+Array.prototype.meuMap = function (callback){
+    const novoArrayMapeado = []
+    for(let indice = 0; indice <= this.length -1; indice ++){
+        const resultado = callback(this[indice], indice)
+        novoArrayMapeado.push(resultado)
+    }
+    return novoArrayMapeado
+}
+
+async function main() {
+    try {
+        const res = await service.getPeople(`a`)
+        // const names = []
+        // res.results.forEach(function(item) {
+        //     names.push(item.name)
+        // });
+
+        //     const names = res.results.map(function(pessoa){
+        //         return pessoa.name
+        //     })
+        //     console.log(names)
+
+        // const names = res.results.map((pessoa) => pessoa.name)
+
+        const names = res.results.meuMap(function(pessoa, indice){
+            console.log(indice)
+            return pessoa.name
+        })
+
+        console.log(names)
+    }
+
+    catch (error) {
+        console.error('internal error', error)
+    }
+}
+
+main()
