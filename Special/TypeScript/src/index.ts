8f624eafb3aca21c548f2ab2d0b6f83ef9f0f650
Maycon Douglas <msantos@tracfone.com>
2022-12-21 10:17:45 -0300

    Enums, tuple and arrays

--- Special/TypeScript/src/index.ts
+++ Special/TypeScript/src/index.ts
@@ -9,3 +9,30 @@ let arr: any[] = [1, true, 'Hello'];
 
 //tuple
 let person: [number, string, boolean] = [1, 'Maycon', true];
+//Tuple Array
+let employee: [number, string][];
+employee = [
+    [1, 'Maycon'],
+    [2, 'John'],
+    [3, 'Doe']
+];
+
+//union
+let sid: string | number = 300;
+
+//enum
+enum Direction1 {
+    Up = 1,
+    Down,
+    Left,
+    Right
+}
+console.log(Direction1.Down);
+
+enum Direction2 {
+    Up = 'Up',
+    Down = 'Down',
+    Left = 'Left',
+    Right = 'Right'
+}
+
