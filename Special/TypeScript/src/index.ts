b4d8da971b2533f0f16a863d08edc59f4f9a2231
Maycon Douglas <msantos@tracfone.com>
2022-12-21 10:12:17 -0300

    Basic Types for TS + TSconfig

---
+++ Special/TypeScript/src/index.ts
@@ -0,0 +1,10 @@
+//Basic Types
+
+let id: number = 5;
+let company: string = 'Populus IT Solutions';
+let isPublished: boolean = true;
+let x: any = 'Hello';
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, 'Hello'];
+
+
